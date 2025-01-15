d0f14de8e925e2ab8ce8085fb9852d1bf5b53d7e
Eigen-0x41 <70758356+Eigen-0x41@users.noreply.github.com>
2025-01-15 22:47:03 +0900

    メッシュ処理のオーバーロード関数の作成と、その関数でのメッシュ化アルゴリズムの修正。

--- Takochu/smg/obj/ObjectSubData/BMDInfo.cs
+++ Takochu/smg/obj/ObjectSubData/BMDInfo.cs
@@ -3,6 +3,7 @@ using OpenTK.Graphics.OpenGL;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Takochu.fmt;
 using Takochu.io;
 using Takochu.rnd;
@@ -51,7 +52,10 @@ namespace Takochu.smg.obj.ObjectSubData
             {
                 TriangleDataList.Add(trianglesPosition);
             }
-
+            public void AddRangeTriangleDataList(BMDTriangleData data)
+            {
+                TriangleDataList.AddRange(data.TriangleDataList);
+            }
         }
 
         public static AbstractObj TargetObject { get; private set; }
@@ -133,6 +137,7 @@ namespace Takochu.smg.obj.ObjectSubData
 
 
         //}
+        [Obsolete]
         public static BMDTriangleData GetTriangles(AbstractObj obj)
         {
             BMDTriangleData bmdTriangleData = new BMDTriangleData();
@@ -202,6 +207,61 @@ namespace Takochu.smg.obj.ObjectSubData
             }
             return bmdTriangleData;
         }
+        public static BMDTriangleData GetTriangles(BMD model)
+        {
+            BMDTriangleData bmdTriangleData = new BMDTriangleData();
+
+                //Batchファイル内のPacketsを取得
+                foreach (BMD.Batch batch in model.Batches)
+                {
+                    Matrix4[] lastmatrixtable = null;
+
+                    foreach (BMD.Batch.Packet packet in batch.Packets)
+                    {
+                        Matrix4[] mtxtable = new Matrix4[packet.MatrixTable.Length];
+                        int[] mtx_debug = new int[packet.MatrixTable.Length];
+
+                        for (int i = 0; i < packet.MatrixTable.Length; i++)
+                        {
+                            if (packet.MatrixTable[i] == 0xFFFF)
+                            {
+                                mtxtable[i] = lastmatrixtable[i];
+                                mtx_debug[i] = 2;
+                            }
+                            else
+                            {
+                                BMD.MatrixType mtxtype = model.MatrixTypes[packet.MatrixTable[i]];
+
+                                if (mtxtype.IsWeighted)
+                                {
+                                    mtxtable[i] = Matrix4.Identity;
+
+                                    mtx_debug[i] = 1;
+                                }
+                                else
+                                {
+                                    mtxtable[i] = model.Joints[mtxtype.Index].FinalMatrix;
+                                    mtx_debug[i] = 0;
+                                }
+                            }
+                        }
+
+                        lastmatrixtable = mtxtable;
+
+
+
+
+                        //ジオメトリ情報にアクセスしていると思われる。
+                        foreach (BMD.Batch.Packet.Primitive prim in packet.Primitives)
+                        {
+                            bmdTriangleData.TriangleDataList.AddRange(GetTriangleFaces(prim, model, mtxtable));
+
+                        }
+                    }
+                }
+            
+            return bmdTriangleData;
+        }
 
         private static void DebugTriangleRendering(BMDTriangleData bmdTriangleData)
         {
@@ -228,7 +288,7 @@ namespace Takochu.smg.obj.ObjectSubData
             Debug.WriteLine($"TargetObjectPosition : {TargetObject.mTruePosition}");
         }
 
-        private static List<BMDTriangleData.TrianglesPosition> GetTriangleFaces(AbstractObj abstructObj,BMD.Batch.Packet.Primitive prim, BMD bmd, Matrix4[] mtxtable)
+        private static List<BMDTriangleData.TrianglesPosition> GetTriangleFaces(AbstractObj abstructObj, BMD.Batch.Packet.Primitive prim, BMD bmd, Matrix4[] mtxtable)
         {
             //Debug.WriteLine(beginMode[(prim.PrimitiveType - 0x80) / 8]);
 
@@ -328,6 +388,114 @@ namespace Takochu.smg.obj.ObjectSubData
 
                         //Debug.WriteLine(pos);
 
+                        trianglesPositionList.Add(new BMDTriangleData.TrianglesPosition(trianglePositionArray));
+                    }
+
+
+                    break;
+                case BeginMode.TriangleFan:
+                    throw new NotSupportedException($"{beginMode[(prim.PrimitiveType - 0x80) / 8]}の形状データは未対応です。");
+                default:
+                    throw new NotSupportedException($"{beginMode[(prim.PrimitiveType - 0x80) / 8]}の形状データは未対応です。");
+
+
+            }
+
+
+
+            return trianglesPositionList;
+        }
+
+    private static List<BMDTriangleData.TrianglesPosition> GetTriangleFaces(BMD.Batch.Packet.Primitive prim, BMD bmd, Matrix4[] mtxtable)
+        {
+            int triangleVertCount = 3;
+            //Debug.WriteLine(beginMode[(prim.PrimitiveType - 0x80) / 8]);
+
+            List<BMDTriangleData.TrianglesPosition> trianglesPositionList = new List<BMDTriangleData.TrianglesPosition>();
+            switch (beginMode[(prim.PrimitiveType - 0x80) / 8])
+            {
+                case BeginMode.Triangles:
+                    for (int vertexIndex = 0; vertexIndex < prim.NumIndices; vertexIndex++)
+                    {
+                        Vector4[] trianglePositionArray = new Vector4[3];
+                        // Triangleなら3固定？
+                        foreach (var triangleVertIndex in Enumerable.Range(0, prim.NumIndices))
+                        {
+                            //頂点情報
+                            //頂点インデックスにあった頂点番号の頂点を順番にセット
+                            trianglePositionArray[triangleVertIndex] = new Vector4(bmd.PositionArray[prim.PositionIndices[vertexIndex]], 1.0f);
+
+                            //モデルの拡大縮小、回転、移動を頂点ごとに適用(モデルビュープロジェクション行列でやるのは適さないから しかし、CPUで計算するので負荷高い)
+                            if ((prim.ArrayMask & 1) != 0)
+                                Vector4.Transform(ref trianglePositionArray[triangleVertIndex],
+                                    ref mtxtable[prim.PosMatrixIndices[vertexIndex]],
+                                    out trianglePositionArray[triangleVertIndex]);
+                            else
+                                Vector4.Transform(ref trianglePositionArray[triangleVertIndex],
+                                    ref mtxtable[0],
+                                    out trianglePositionArray[triangleVertIndex]);
+
+                            // Debug.WriteLine(pos);
+                            // Debug.WriteLine("通常△");
+                        }
+                        trianglesPositionList.Add(new BMDTriangleData.TrianglesPosition(trianglePositionArray));
+                    }
+                    break;
+                case BeginMode.TriangleStrip:
+                    for (int vertexIndex = 2; vertexIndex < prim.NumIndices;)
+                    {
+                        // 一つの線の両端から次の頂点を回転方向を変更することで三角面を取得します。
+                        // この処理を線ごとにするため、index初期値は線の頂点数となる。
+                        vertexIndex -= 2;
+                        Vector4[] trianglePositionArray = new Vector4[3];
+                        if ((vertexIndex % 2) != 0) 
+                        {
+                            // 順方向
+                            foreach (var triangleVertIndex in Enumerable.Range(0, triangleVertCount))
+                            {
+                                //頂点情報
+                                //頂点インデックスにあった頂点番号の頂点を順番にセット
+                                trianglePositionArray[triangleVertIndex] = new Vector4(bmd.PositionArray[prim.PositionIndices[vertexIndex]], 1.0f);
+
+                                //モデルの拡大縮小、回転、移動を頂点ごとに適用(モデルビュープロジェクション行列でやるのは適さないから しかし、CPUで計算するので負荷高い)
+                                if ((prim.ArrayMask & 1) != 0)
+                                    Vector4.Transform(ref trianglePositionArray[triangleVertIndex],
+                                        ref mtxtable[prim.PosMatrixIndices[vertexIndex]],
+                                        out trianglePositionArray[triangleVertIndex]);
+                                else
+                                    Vector4.Transform(ref trianglePositionArray[triangleVertIndex],
+                                        ref mtxtable[0],
+                                        out trianglePositionArray[triangleVertIndex]);
+
+                                // Debug.WriteLine(pos);
+                                // Debug.WriteLine("通常△");
+                                vertexIndex++;
+                            }
+                        }
+                        else
+                        {
+                            // 逆方向
+                            foreach (var triangleVertIndex in Enumerable.Range(0, triangleVertCount).Reverse())
+                            {
+                                //頂点情報
+                                //頂点インデックスにあった頂点番号の頂点を順番にセット
+                                trianglePositionArray[triangleVertIndex] = new Vector4(bmd.PositionArray[prim.PositionIndices[vertexIndex]], 1.0f);
+
+                                //モデルの拡大縮小、回転、移動を頂点ごとに適用(モデルビュープロジェクション行列でやるのは適さないから しかし、CPUで計算するので負荷高い)
+                                if ((prim.ArrayMask & 1) != 0)
+                                    Vector4.Transform(ref trianglePositionArray[triangleVertIndex],
+                                        ref mtxtable[prim.PosMatrixIndices[vertexIndex]],
+                                        out trianglePositionArray[triangleVertIndex]);
+                                else
+                                    Vector4.Transform(ref trianglePositionArray[triangleVertIndex],
+                                        ref mtxtable[0],
+                                        out trianglePositionArray[triangleVertIndex]);
+
+                                // Debug.WriteLine(pos);
+                                // Debug.WriteLine("通常△");
+                                vertexIndex++;
+                            }
+                        }
                         trianglesPositionList.Add(new BMDTriangleData.TrianglesPosition(trianglePositionArray));
                     }
 
